2818969998a82b95ca088c4f3a0b70e49ce3e4b0
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-06-17 10:41:50 -0400

    tailwindcss installation and creating initial views

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="w-full h-screen p-4 flex gap-5 flex-col justify-center items-center bg-neutral-950 text-white">
+        <header class="flex flex-col items-center">
+            <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="75" height="75" />
+            <div class="text-center mt-4">
+                <RouterLink :to="{ name: 'home' }" class="text-3xl font-semibold block mb-2">
+                    VAPP
+                </RouterLink>
+                <div>
+                    An simple Vue Application
+                </div>
+            </div>
+        </header>
+        <div class="w-full">
+            <main>
+                <RouterView />
+            </main>
+        </div>
+        <footer class="text-sm text-neutral-600 font-medium">
+            Developed by <a class="text-violet-900 hover:text-violet-800 duration-200" href="https://github.com/ernandsrs" target="_blank"
+                title="Profile on Github">Ernands RS</a>
+        </footer>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,31 @@
+<template>
+    <div class="bg-neutral-900 rounded-lg p-5 w-full max-w-[475px] mx-auto">
+
+        <div class="flex justify-center gap-2.5">
+            <RouterLink class="bg-violet-950 hover:bg-violet-900 duration-200 px-6 py-2.5 rounded-lg" v-for="(link, index) in links" :key="index"
+                :to="link.to">
+                {{ link.label }}
+            </RouterLink>
+        </div>
+
+    </div>
+</template>
+
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
+const links = [
+    {
+        icon: 'home',
+        label: 'Home',
+        to: { name: 'home' }
+    },
+    {
+        icon: 'login',
+        label: 'Login',
+        to: { name: 'home' }
+    }
+];
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped></style>
