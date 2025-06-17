e49942eef1ee99c7ddd8b2ba8e11ca7d72f09722
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-06-17 10:57:42 -0400

    bootstrap icons installation/creating a simple icon component

---
+++ src/components/ui/CIcon.vue
@@ -0,0 +1,21 @@
+<template>
+    <i :class="'inline-block bi bi-' + props.name + (props.prepend ? ' mr-2.5' : (props.append ? ' ml-2.5' : ' '))"></i>
+</template>
+
+<script setup lang="ts">
+
+export interface IconProps {
+    name: string,
+    prepend?: boolean,
+    append?: boolean,
+};
+
+const props = withDefaults(defineProps<IconProps>(), {
+    name: 'app',
+    prepend: false,
+    append: false,
+});
+
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,12 @@
     <div class="bg-neutral-900 rounded-lg p-5 w-full max-w-[475px] mx-auto">
 
         <div class="flex justify-center gap-2.5">
-            <RouterLink class="bg-violet-950 hover:bg-violet-900 duration-200 px-6 py-2.5 rounded-lg" v-for="(link, index) in links" :key="index"
-                :to="link.to">
-                {{ link.label }}
+            <RouterLink class="bg-violet-950 hover:bg-violet-900 duration-200 px-6 py-2.5 rounded-lg"
+                v-for="(link, index) in links" :key="index" :to="link.to">
+                <CIcon prepend :name="link.icon" />
+                <span class=" inline-block">
+                    {{ link.label }}
+                </span>
             </RouterLink>
         </div>
 
@@ -13,14 +16,16 @@
 
 <script setup lang="ts">
 
+import CIcon from '@/components/ui/CIcon.vue';
+
 const links = [
     {
-        icon: 'home',
+        icon: 'house-door',
         label: 'Home',
         to: { name: 'home' }
     },
     {
-        icon: 'login',
+        icon: 'box-arrow-in-right',
         label: 'Login',
         to: { name: 'home' }
     }
