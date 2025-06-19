1a63c32727e29986ffd5753c3508d64a3f366790
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-06-19 18:14:48 -0400

    auth form using form components

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -5,17 +5,45 @@
         </div>
 
         <div>
-            <input type="text">
+            <CForm :data="loginForm" :validation-schema="loginValidationSchema" submit-text="Login">
+                <CInput class="col-span-12" v-model="loginForm.email" type="email" label="E-mail" id="email" />
+                <CInput class="col-span-12" v-model="loginForm.password" type="password" label="Password"
+                    id="password" />
+
+                <div class="col-span-12 flex justify-center">
+                    <CToggle v-model="loginForm.remember" label="Remember-me" right-label id="remember" />
+                </div>
+            </CForm>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
 
+import CForm from '@/components/ui/form/CForm.vue';
+import CInput from '@/components/ui/form/CInput.vue';
+import CToggle from '@/components/ui/form/CToggle.vue';
 import { useAppSettings } from '@/composables/useAppSettings';
-import { onMounted } from 'vue';
+import { yupValidator } from '@/utils/validator';
+import { onMounted, ref } from 'vue';
 
 const { setAppTitle } = useAppSettings();
+
+const loginValidationSchema = yupValidator.object({
+    email: yupValidator.string().required('Required field').email('Requires a valid e-mail'),
+    password: yupValidator.string().required('Required field'),
+    remember: yupValidator.boolean()
+});
+
+const loginForm = ref<{
+    email: string,
+    password: string,
+    remember: boolean
+}>({
+    email: '',
+    password: '',
+    remember: false
+});
 
 onMounted(() => {
 
