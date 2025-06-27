9ad75e7537af7d1768070087f96a2ff685588cae
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-06-27 12:45:21 -0400

    icon preview

--- src/views/dash/doc/IconsView.vue
+++ src/views/dash/doc/IconsView.vue
@@ -6,6 +6,8 @@
                 tag: 'h1',
                 title: 'CIcon',
                 texts: [
+                    '<b>CIcon</b> é um componente para exibição de ícones da biblioteca <b>Bootstrap Icons</b>, por isso, ao utilizar este componente é preciso informar um nome de classe do Bootstrap Icons válido.',
+                    'Acesse <i>(https://icons.getbootstrap.com)</i> para a lista de ícones.'
                 ],
                 componentDoc: {
                     props: [
@@ -33,6 +35,52 @@
             }
         ]">
 
+            <template #previews>
+                <div class="grid grid-cols-12 gap-6 w-full">
+                    <div
+                        class="col-span-6 flex flex-col items-center border border-neutral-300 dark:border-neutral-700 rounded-lg p-5">
+                        <div class="font-semibold mb-5">Default</div>
+                        <div class="flex justify-center gap-6 flex-wrapp">
+                            <CIcon name="rocket-takeoff-fill" />
+                            <CIcon name="rocket-takeoff" />
+                        </div>
+                    </div>
+
+                    <div
+                        class="col-span-6 flex flex-col items-center border border-neutral-300 dark:border-neutral-700 rounded-lg p-5">
+                        <div class="font-semibold mb-5">Custom</div>
+                        <div class="flex justify-center gap-6 flex-wrapp">
+                            <div
+                                class="size-10 flex justify-center items-center bg-radial-[at_25%_5%] from-purple-600 to-violet-700 rounded-full">
+                                <CIcon name="rocket-takeoff-fill" />
+                            </div>
+
+                            <div
+                                class="size-10 flex justify-center items-center bg-radial-[at_25%_5%] from-rose-600 to-purple-700 rounded-full shadow-lg dark:shadow-purple-600/25">
+                                <CIcon name="rocket-takeoff" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </template>
+
+            <template #codes>
+                <textarea>
+                    <!-- Default -->
+                    <CIcon name="rocket-takeoff-fill" />
+                    <CIcon name="rocket-takeoff" />
+
+                    <!-- Custom -->
+                    <div class="size-10 flex justify-center items-center bg-radial-[at_25%_5%] from-purple-600 to-violet-700 rounded-full">
+                        <CIcon name="rocket-takeoff-fill" />
+                    </div>
+
+                    <div class="size-10 flex justify-center items-center bg-radial-[at_25%_5%] from-rose-600 to-purple-700 rounded-full shadow-lg dark:shadow-purple-600/25">
+                        <CIcon name="rocket-takeoff" />
+                    </div>
+                </textarea>
+            </template>
+
         </CDocSection>
 
     </CPage>
@@ -42,6 +90,7 @@
 
 import CDocSection from '@/components/dashboard/CDocSection.vue';
 import CPage from '@/components/dashboard/CPage.vue';
+import CIcon from '@/components/ui/CIcon.vue';
 
 </script>
 
