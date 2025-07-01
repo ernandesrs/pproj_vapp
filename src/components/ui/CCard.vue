4ed3c1b1e9d7eed536767250fbefe1d1b75eb717
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-07-01 10:13:00 -0400

    card componnet implementing

---
+++ src/components/ui/CCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="bg-white border border-neutral-200 dark:border-neutral-800 dark:bg-neutral-900 rounded-lg">
+        <div v-if="withHeader" class="bg-white dark:bg-neutral-900 flex items-center gap-2.5 py-5 px-6 border-b border-neutral-200 dark:border-neutral-800 relative z-10 rounded-tl-lg rounded-tr-lg">
+
+            <div class="flex-1">
+                <Component v-if="props.title" :is="props.titleTag" v-html="props.title"
+                    class="text-base font-semibold" />
+                <template v-else-if="$slots.header">
+                    <slot name="header" />
+                </template>
+            </div>
+
+        </div>
+
+        <div class="py-5 px-6 relative z-0">
+            <slot />
+        </div>
+
+        <div v-if="withFooter" class="flex items-center gap-2.5 py-5 px-6 border-t border-neutral-200 dark:border-neutral-800">
+
+            <div class="flex-1">
+                <slot name="footer" />
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import type { CardProps } from '@/types/components/ui/card_type';
+import { computed, useSlots } from 'vue';
+
+const slots = useSlots();
+
+const props = withDefaults(defineProps<CardProps>(), {
+    titleTag: 'h5'
+});
+
+const withHeader = computed((): boolean => {
+    return props.title || slots.header ? true : false;
+});
+
+const withFooter = computed((): boolean => {
+    return slots.footer ? true : false;
+});
+
+</script>
+
+<style scoped></style>
--- src/views/dash/doc/CardsView.vue
+++ src/views/dash/doc/CardsView.vue
@@ -3,9 +3,91 @@
         <CDocSection :data="[
             {
                 tag: 'h1',
-                title: 'CCard'
+                title: 'CCard',
+                texts: [
+                    'Card component.'
+                ],
+                componentDoc: {
+                    slots: [
+                        {
+                            name: 'default',
+                            desc: 'Default slot.'
+                        },
+                        {
+                            name: 'header',
+                            desc: 'Slot to a custom header component.'
+                        },
+                        {
+                            name: 'footer',
+                            desc: 'Slot to a custom footer component.'
+                        }
+                    ],
+                    props: [
+                        {
+                            name: 'titleTag',
+                            type: 'string',
+                            required: false,
+                            desc: 'Title tag.',
+                            allowedValues: '---',
+                            defaultValue: 'h5'
+                        },
+                        {
+                            name: 'title',
+                            type: 'string',
+                            required: false,
+                            desc: 'Card title.',
+                            allowedValues: '---',
+                            defaultValue: '---'
+                        }
+                    ]
+                }
             }
-        ]"></CDocSection>
+        ]">
+
+            <template #previews>
+                <div class="grid grid-cols-12 gap-5">
+
+                    <CCard class="col-span-12 sm:col-span-6">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laborum cum veniam ad,
+                        consequuntur laborum cum veniam ad, consequuntur.
+                    </CCard>
+
+                    <CCard title="Lorem title" class="col-span-12 sm:col-span-6">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laborum cum veniam ad,
+                        consequuntur laborum cum veniam ad, consequuntur.
+                    </CCard>
+
+                    <CCard class="col-span-12 sm:col-span-6">
+                        <template #header>
+                            <div class="flex items-center">
+                                <CIcon name="rocket-fill" />
+                                <div class="font-semibold ml-2.5">
+                                    Custom header
+                                </div>
+                            </div>
+                        </template>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laborum cum veniam ad,
+                        consequuntur laborum cum veniam ad, consequuntur.
+                    </CCard>
+
+                    <CCard class="col-span-12 sm:col-span-6">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laborum cum veniam ad,
+                        consequuntur laborum cum veniam ad, consequuntur.
+
+                        <template #footer>
+                            <div class="flex items-center">
+                                <CIcon name="rocket-fill" />
+                                <div class="font-semibold ml-2.5">
+                                    Footer
+                                </div>
+                            </div>
+                        </template>
+                    </CCard>
+
+                </div>
+            </template>
+
+        </CDocSection>
     </CPage>
 </template>
 
@@ -13,6 +95,8 @@
 
 import CDocSection from '@/components/dashboard/CDocSection.vue';
 import CPage from '@/components/dashboard/CPage.vue';
+import CCard from '@/components/ui/CCard.vue';
+import CIcon from '@/components/ui/CIcon.vue';
 
 </script>
 
