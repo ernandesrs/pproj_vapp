724c54682b6d11cb24fac14e526d11deb63e0827
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-07-06 12:04:09 -0400

    theme in app store

--- src/composables/useApp.ts
+++ src/composables/useApp.ts
@@ -4,27 +4,13 @@ import { useAppStore } from "@/stores/app";
 
 const MOBILE_WIDTH = 1024;
 
-const getThemeFromStorage = (): string => {
-    return localStorage.getItem('theme') ?? 'light';
-};
-
-const setAndStoreThemeOnStorage = (theme: string): void => {
-    const htmlElement = document.querySelector('html');
-
-    if (htmlElement) {
-        htmlElement.setAttribute('class', theme);
-    }
-
-    localStorage.setItem('theme', theme);
-};
-
 export function useApp() {
     const appStore = useAppStore();
 
     const windowWidth = ref<number>(window.innerWidth);
     const inMobile = computed(() => windowWidth.value < MOBILE_WIDTH);
     const showSidebar = ref<boolean>(!inMobile.value);
-    const darkMode = ref<boolean>(getThemeFromStorage() === 'dark');
+    const darkMode = ref<boolean>(appStore.themeName === 'dark');
     const inLoadingMode = computed(() => appStore.loading);
 
     const { addToast } = useToast();
@@ -32,8 +18,6 @@ export function useApp() {
     window.addEventListener('resize', () => {
         windowWidth.value = window.innerWidth;
     });
-
-    setAndStoreThemeOnStorage(getThemeFromStorage());
 
     const setAppTitle = (title: string): void => {
         const baseTitle = document.title.split('|')[0];
@@ -50,12 +34,11 @@ export function useApp() {
     };
 
     const isDarkTheme = (): boolean => {
-        return getThemeFromStorage() === 'dark';
+        return appStore.themeName === 'dark';
     };
 
     const darkModeToggle = (): void => {
-        const newTheme = getThemeFromStorage() === 'dark' ? 'light' : 'dark';
-        setAndStoreThemeOnStorage(newTheme);
+        appStore.themeName = appStore.themeName === 'dark' ? 'light' : 'dark';
     };
 
     watch(() => darkMode.value, (n) => {
--- src/stores/app.ts
+++ src/stores/app.ts
@@ -1,10 +1,32 @@
 import { defineStore } from "pinia";
-import { ref } from "vue";
+import { ref, watch } from "vue";
+
+const getThemeFromStorage = (): string => {
+    return localStorage.getItem('theme') ?? 'light';
+};
+
+const setAndStoreThemeOnStorage = (theme: string): void => {
+    const htmlElement = document.querySelector('html');
+
+    if (htmlElement) {
+        htmlElement.setAttribute('class', theme);
+    }
+
+    localStorage.setItem('theme', theme);
+};
 
 export const useAppStore = defineStore('app', () => {
     const loading = ref<boolean>(false);
+    const themeName = ref<string>(getThemeFromStorage());
+
+    setAndStoreThemeOnStorage(getThemeFromStorage());
+
+    watch(() => themeName.value, (newThemeName) => {
+        setAndStoreThemeOnStorage(newThemeName);
+    }, { immediate: true });
 
     return {
         loading,
+        themeName,
     };
 });
