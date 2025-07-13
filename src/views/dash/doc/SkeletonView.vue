65102771dd0c3612df592b7d0b24a8053bb16b0e
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-07-13 11:55:38 -0400

    documenting skeleton components

--- src/views/dash/doc/SkeletonView.vue
+++ src/views/dash/doc/SkeletonView.vue
@@ -3,7 +3,50 @@
         <CDocSection :data="[
             {
                 tag: 'h1',
-                title: 'CSkeleton'
+                title: 'CSkeleton and CSkeletonText',
+                texts: [
+                    'The two components, to be used to show the user that content is loading.',
+                    'The components has a <i><b>position: absolute</b></i> and a <i><b>z-index: 999</b></i>, so it needs to be placed inside a container with <i><b>position: relative</b></i>, and all content inside this container will be overlapped by the <i><b>skeleton</b></i>.',
+                ]
+            }
+        ]" />
+
+        <CDocSection :data="[
+            {
+                tag: 'h1',
+                title: 'CSkeleton',
+                texts: [
+                    'By default, it occupies 100% of the container, but its size can be customized via the <i>prop <b>customTailwindSizeClass</b></i>, which must be a valid Tailwind CSS class.',
+                    'See more details below.'
+                ],
+                componentDoc: {
+                    props: [
+                        {
+                            name: 'show',
+                            type: 'boolean',
+                            required: false,
+                            desc: 'When true, the skeleton is displayed.',
+                            allowedValues: '---',
+                            defaultValue: '---'
+                        },
+                        {
+                            name: 'circle',
+                            type: 'boolean',
+                            required: false,
+                            desc: 'When true, the skeleton is a circle.',
+                            allowedValues: '---',
+                            defaultValue: '---'
+                        },
+                        {
+                            name: 'customTailwindSizeClass',
+                            type: 'string',
+                            required: false,
+                            desc: 'Custom size. Requires a valid Tailwind CSS class.',
+                            allowedValues: '---',
+                            defaultValue: 'w-full h-full'
+                        }
+                    ]
+                }
             }
         ]">
             <template #previews>
@@ -42,6 +85,77 @@
 
                     </div>
 
+                </div>
+            </template>
+
+            <template #codes>
+                <textarea>
+                        <!-- Filled container -->
+                        <div class="w-full h-16 relative">
+                            <CSkeleton show />
+                        </div>
+
+                        <!-- Custom size -->
+                        <div class="relative">
+                            <CSkeleton show custom-tailwind-size-class="w-28 h-16" />
+                        </div>
+
+                        <!-- Circle -->
+                        <div class="relative">
+                            <CSkeleton show custom-tailwind-size-class="size-16" circle />
+                        </div></textarea>
+            </template>
+        </CDocSection>
+
+        <CDocSection :data="[
+            {
+                tag: 'h1',
+                title: 'CSkeletonText',
+                texts: [
+                    'Skeleton component for texts/paragraphs.',
+                    'You can customize the quantity, height and space between lines, see the properties below.'
+                ],
+                componentDoc: {
+                    props: [
+                        {
+                            name: 'show',
+                            type: 'boolean',
+                            required: false,
+                            desc: 'When true, the skeleton is displayed.',
+                            allowedValues: '---',
+                            defaultValue: '---'
+                        },
+                        {
+                            name: 'lines',
+                            type: 'number',
+                            required: false,
+                            desc: 'Number of lines.',
+                            allowedValues: '---',
+                            defaultValue: '1'
+                        },
+                        {
+                            name: 'lineHeight',
+                            type: 'string',
+                            required: false,
+                            desc: 'Height of each row. Requires a valid Tailwind CSS class.',
+                            allowedValues: 'h-*',
+                            defaultValue: 'h-2'
+                        },
+                        {
+                            name: 'lineGap',
+                            type: 'string',
+                            required: false,
+                            desc: 'Spacing between each line. Requires a valid Tailwind CSS class.',
+                            allowedValues: 'gap-*, gap-y-*',
+                            defaultValue: 'gap-2.5'
+                        }
+                    ]
+                }
+            }
+        ]">
+            <template #previews>
+                <div class="flex-1 grid grid-cols-12 gap-8">
+
                     <div class="col-span-6 md:col-span-4 xl:col-span-6">
                         <div class="font-medium mb-2.5 text-neutral-400 dark:text-neutral-500">
                             Text default
@@ -58,13 +172,25 @@
                             Text custom height and gap
                         </div>
 
-                        <div class="relative">
+                        <div class="relative h-[90px]">
                             <CSkeletonText show :lines="3" line-height="h-4" line-gap="gap-4" />
                         </div>
 
                     </div>
 
                 </div>
+            </template>
+            <template #codes>
+                <textarea>
+                        <!-- Text default -->
+                        <div class="relative">
+                            <CSkeletonText show :lines="5" />
+                        </div>
+
+                        <!-- Text custom height and gap -->
+                        <div class="relative">
+                            <CSkeletonText show :lines="3" line-height="h-4" line-gap="gap-4" />
+                        </div></textarea>
             </template>
         </CDocSection>
     </CPage>
