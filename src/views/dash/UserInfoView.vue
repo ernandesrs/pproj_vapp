5350a2db84bc6e3c819593d5ecfc7b3a1994ca62
Ernandes R Souza <ernandesrsouza@gmail.com>
2025-07-25 11:29:43 -0400

    user info demo view

--- src/views/dash/UserInfoView.vue
+++ src/views/dash/UserInfoView.vue
@@ -1,15 +1,49 @@
 <template>
     <CPage :loading="loading" title="User Info">
         <CPageSection with-grid>
-            <div class="col-span-12">
-                {{ user }}
-            </div>
+            <CCard class="col-span-12">
+
+
+                <div class="grid grid-cols-12 gap-3">
+                    <div class="col-span-12 flex flex-col items-center mb-3">
+                        <CSkeleton v-if="loading" variant="circle" width="w-36" height="h-36" />
+                        <CThumbnail v-else :image="user?.image" :text="(user?.name as string ?? '')" size="xl" square
+                            circle />
+                    </div>
+
+                    <CLabeledValue class="col-span-6" label="Name" :value="user?.name" :loading="loading" />
+                    <CLabeledValue class="col-span-6" label="Username" :value="user?.username" :loading="loading" />
+
+                    <CLabeledValue class="col-span-6" label="E-mail" :value="user?.email" :loading="loading" />
+                    <CLabeledValue class="col-span-6" label="DoB" :value="user?.dob" :loading="loading">
+                        <template #prepend>
+                            <CTooltip width="w-[145px]">
+                                <CIcon name="info-circle" />
+                                <template #tooltip>
+                                    <div class="flex flex-col items-center gap-2.5">
+                                        <CIcon name="cake2-fill" class="text-2xl" />
+                                        <p>User's birth day.</p>
+                                    </div>
+                                </template>
+                            </CTooltip>
+                        </template>
+                    </CLabeledValue>
+                </div>
+
+            </CCard>
+
         </CPageSection>
     </CPage>
 </template>
 
 <script setup lang="ts">
 
+import CCard from '@/components/ui/CCard.vue';
+import CIcon from '@/components/ui/CIcon.vue';
+import CLabeledValue from '@/components/ui/CLabeledValue.vue';
+import CSkeleton from '@/components/ui/CSkeleton.vue';
+import CThumbnail from '@/components/ui/CThumbnail.vue';
+import CTooltip from '@/components/ui/CTooltip.vue';
 import CPage from '@/components/ui/layout/CPage.vue';
 import CPageSection from '@/components/ui/layout/CPageSection.vue';
 import { useApp } from '@/composables/useApp';
@@ -39,7 +73,7 @@ const getUser = async (): Promise<null | User> => {
             username: r.username,
             dob: r.birthDate,
             email: r.email,
-            photo: r.image,
+            image: r.image,
             original: r
         } as User;
     } catch {
